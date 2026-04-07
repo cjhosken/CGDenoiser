3609195483fe33c3ac3a85a10d8e32fa95d57f94
Christopher Hosken <hoskenchristopher@gmail.com>
2026-04-07 23:40:03 +0100

    windows setup

---
+++ building/build.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+:: Get the directory of this script
+set SCRIPT_DIR=%~dp0
+
+:: Remove trailing backslash (optional but cleaner)
+if "%SCRIPT_DIR:~-1%"=="\" set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
+cd /d "%SCRIPT_DIR%"
+
+:: Create directory (mkdir handles existing dirs fine)
+mkdir "%SCRIPT_DIR%\..\lib\bin"
+
+:: Run CMake configure
+cmake -S . -B "..\build" -Wno-dev
+
+:: Build with Release config
+cmake --build "..\build" --config Release -- /m:16
+
+endlocal
--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ rm -rf ./build
 
 export Nuke_ROOT=/home/hoske/software/Foundry/Nuke15.2v4 # Replace with your Nuke installation path
 
-cmake -S . -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S . -B ./build -Wno-dev
 
 cmake --build ./build --target install
