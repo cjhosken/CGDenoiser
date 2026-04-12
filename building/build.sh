80f693dc734beff9fa431a05685a16b08d4e0663
Christopher Hosken <hoskenchristopher@gmail.com>
2026-04-12 14:10:05 +0100

    updated linux build

--- building/build.sh
+++ building/build.sh
@@ -2,7 +2,7 @@
 export SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$SCRIPT_DIR"
 
-mkdir -p $SCRIPT_DIR/../lib/{bin}
+mkdir -p $SCRIPT_DIR/../lib/bin
 
 cmake -S . -B ../build
 
